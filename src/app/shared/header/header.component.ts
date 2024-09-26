6637856967709ac119d250906ee0d18eec9bed14
Anna Fritz <anna_eida@gmx.de>
2024-09-26 22:56:55 +0200

    added header navbar with styling & functions

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,12 +1,26 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  currantLanguage = 'DE';
+  toggleDirection = 'right';
 
+  toggleLanguage() {
+    if (this.currantLanguage == 'DE') {
+      this.currantLanguage = 'EN';
+      this.toggleDirection = 'left';
+    } else {
+      this.currantLanguage = 'DE';
+      this.toggleDirection = 'right';
+    }
+  }
+  
 }
+
