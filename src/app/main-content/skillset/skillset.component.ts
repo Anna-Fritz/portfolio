bb06b4b65cbcc8ba5b9333efe89960b28a9cd466
Anna Fritz <anna_eida@gmx.de>
2024-09-30 11:13:07 +0200

    added skills icons section

--- src/app/main-content/skillset/skillset.component.ts
+++ src/app/main-content/skillset/skillset.component.ts
@@ -8,4 +8,55 @@ import { Component } from '@angular/core';
   styleUrl: './skillset.component.scss'
 })
 export class SkillsetComponent {
+  
+  skillIcons: {
+    name: string,
+    src: string
+  } [] = [
+    {
+      name: 'HTML',
+      src: './../../../assets/icons/skills/html.svg'
+    },
+    {
+      name: 'CSS',
+      src: './../../../assets/icons/skills/css.svg'
+    },
+    {
+      name: 'JavaScript',
+      src: './../../../assets/icons/skills/javascript.svg'
+    },
+    {
+      name: 'Material Design',
+      src: './../../../assets/icons/skills/material_design.svg'
+    },
+    {
+      name: 'TypeScript',
+      src: './../../../assets/icons/skills/typescript.svg'
+    },
+    {
+      name: 'Angular',
+      src: './../../../assets/icons/skills/angular.svg'
+    },
+    {
+      name: 'Firebase',
+      src: './../../../assets/icons/skills/firebase.svg'
+    },
+    {
+      name: 'Git',
+      src: './../../../assets/icons/skills/git.svg'
+    },
+    {
+      name: 'Rest-Api',
+      src: './../../../assets/icons/skills/api.svg'
+    },
+    {
+      name: 'Scrum',
+      src: './../../../assets/icons/skills/scrum.svg'
+    },
+    {
+      name: 'Growth Mindset',
+      src: './../../../assets/icons/skills/growth_mindset.svg'
+    }
+  ]
+
 }
