d4de92b7e21f2b59c332e807a1cf3b45b2f871f4
Anna Fritz <anna_eida@gmx.de>
2024-10-02 14:40:43 +0200

    fixed responsive for large screens over 1440px

--- src/app/main-content/projects/single-project/single-project.component.ts
+++ src/app/main-content/projects/single-project/single-project.component.ts
@@ -20,10 +20,8 @@ export class SingleProjectComponent {
   showNextProject(index: number) {
     if(index == this.projectdata.project.length - 1) {
       this.projectdata.index = 0;
-      console.log("index", index)
     } else {
       this.projectdata.index = index + 1;
-      console.log("index", index)
     }
   }
 }
