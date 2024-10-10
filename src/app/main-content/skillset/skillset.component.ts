88c281144cee07951d5bd4acbb7e1a3c992eeb46
Anna Fritz <anna_eida@gmx.de>
2024-10-10 02:39:17 +0200

    fixed image paths for ftp & small changes

--- src/app/main-content/skillset/skillset.component.ts
+++ src/app/main-content/skillset/skillset.component.ts
@@ -15,55 +15,55 @@ export class SkillsetComponent {
   } [] = [
     {
       name: 'JavaScript',
-      src: './../../../assets/icons/skills/javascript.svg'
+      src: 'assets/icons/skills/javascript.svg'
     },
     {
       name: 'HTML',
-      src: './../../../assets/icons/skills/html.svg'
+      src: 'assets/icons/skills/html.svg'
     },
     {
       name: 'CSS',
-      src: './../../../assets/icons/skills/css.svg'
+      src: 'assets/icons/skills/css.svg'
     },
     {
       name: 'Angular',
-      src: './../../../assets/icons/skills/angular.svg'
+      src: 'assets/icons/skills/angular.svg'
     },
     {
       name: 'TypeScript',
-      src: './../../../assets/icons/skills/typescript.svg'
+      src: 'assets/icons/skills/typescript.svg'
     },
     {
       name: 'SASS/SCSS',
-      src: './../../../assets/icons/skills/sass.svg'
+      src: 'assets/icons/skills/sass.svg'
     },
     {
       name: 'Firebase',
-      src: './../../../assets/icons/skills/firebase.svg'
+      src: 'assets/icons/skills/firebase.svg'
     },
     {
       name: 'Git',
-      src: './../../../assets/icons/skills/git.svg'
+      src: 'assets/icons/skills/git.svg'
     },
     {
       name: 'Rest-Api',
-      src: './../../../assets/icons/skills/api.svg'
+      src: 'assets/icons/skills/api.svg'
     },
     {
       name: 'Scrum',
-      src: './../../../assets/icons/skills/scrum.svg'
+      src: 'assets/icons/skills/scrum.svg'
     },
     {
       name: 'Material Design',
-      src: './../../../assets/icons/skills/material_design.svg'
+      src: 'assets/icons/skills/material_design.svg'
     },
     {
       name: 'Figma',
-      src: './../../../assets/icons/skills/figma.svg'
+      src: 'assets/icons/skills/figma.svg'
     },
     {
       name: 'Bootstrap',
-      src: './../../../assets/icons/skills/bootstrap.svg'
+      src: 'assets/icons/skills/bootstrap.svg'
     },
   ]
 }
