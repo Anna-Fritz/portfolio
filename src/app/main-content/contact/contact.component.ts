04cba669a3c13db91ccb1dda5e581e94b8634a74
Anna Fritz <anna_eida@gmx.de>
2024-10-11 17:00:32 +0200

    completed function onSubmit() for sending mails to own domain mail

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -3,6 +3,7 @@ import { TranslationService } from '../../translation.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-contact',
@@ -14,6 +15,7 @@ import { CommonModule } from '@angular/common';
 export class ContactComponent {
 
   translate = inject(TranslationService);
+  http = inject(HttpClient);
 
   contactData: {
     name: string,
@@ -25,14 +27,39 @@ export class ContactComponent {
     message: ""
   }
 
+  mailTest = true;
+
+  post = {
+    endPoint: 'https://deineDomain.de/sendMail.php',
+    body: (payload: any) => JSON.stringify(payload),
+    options: {
+      headers: {
+        'Content-Type': 'text/plain',
+        responseType: 'text',
+      },
+    },
+  };
+
   onSubmit(ngForm: NgForm) {
-    if (ngForm.valid && ngForm.submitted) {
-      console.log(this.contactData);
+    if (ngForm.submitted && ngForm.form.valid && !this.mailTest) {
+      this.http.post(this.post.endPoint, this.post.body(this.contactData))
+        .subscribe({
+          next: (response) => {
+
+            ngForm.resetForm();
+          },
+          error: (error) => {
+            console.error(error);
+          },
+          complete: () => console.info('send post complete'),
+        });
+    } else if (ngForm.submitted && ngForm.form.valid && this.mailTest) {
+
+      ngForm.resetForm();
     }
   }
 
   showInput(inputfield: HTMLElement) {
     inputfield.style.display = "block";
-
   }
 }
