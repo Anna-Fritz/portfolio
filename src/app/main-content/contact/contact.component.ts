65a3b1e545cefb6cbe64132ad267eae807eb6e86
Anna Fritz <anna_eida@gmx.de>
2024-10-11 14:24:20 +0200

    added contact form validation

--- src/app/main-content/contact/contact.component.ts
+++ src/app/main-content/contact/contact.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject } from '@angular/core';
 import { TranslationService } from '../../translation.service';
 import { TranslateModule } from '@ngx-translate/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [TranslateModule],
+  imports: [TranslateModule, FormsModule, CommonModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
@@ -13,9 +15,24 @@ export class ContactComponent {
 
   translate = inject(TranslationService);
 
+  contactData: {
+    name: string,
+    email: string,
+    message: string
+  } = {
+    name: "",
+    email: "",
+    message: ""
+  }
 
-  test() {
-    console.log("mega!");
-    
+  onSubmit(ngForm: NgForm) {
+    if (ngForm.valid && ngForm.submitted) {
+      console.log(this.contactData);
+    }
+  }
+
+  showInput(inputfield: HTMLElement) {
+    inputfield.style.display = "block";
+
   }
 }
