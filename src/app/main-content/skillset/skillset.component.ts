d894ec89a58be3610ea0facac8b5b50bca006bd8
Anna Fritz <anna_eida@gmx.de>
2025-05-20 13:14:50 +0200

    feat: add option to switch stack icons from frontend to backend

--- src/app/main-content/skillset/skillset.component.ts
+++ src/app/main-content/skillset/skillset.component.ts
@@ -3,11 +3,12 @@ import { TranslationService } from '../../translation.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { ProjectdataService } from '../../projectdata.service';
 import { RouterLink } from '@angular/router';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-skillset',
   standalone: true,
-  imports: [TranslateModule, RouterLink],
+  imports: [TranslateModule, RouterLink, NgClass],
   templateUrl: './skillset.component.html',
   styleUrl: './skillset.component.scss'
 })
@@ -15,6 +16,9 @@ export class SkillsetComponent {
 
   translate = inject(TranslationService);
   projectdata = inject(ProjectdataService);
+
+  stackFrontend = true;
+  stackBackend = false;
   
   skillIcons: {
     name: string,
@@ -73,4 +77,72 @@ export class SkillsetComponent {
       src: 'assets/icons/skills/bootstrap.svg'
     },
   ];
+
+  skillIconsBackend: {
+    name: string,
+    src: string
+  } [] = [
+    {
+      name: 'Python',
+      src: 'assets/icons/skills_backend/python.svg'
+    },
+    {
+      name: 'Django',
+      src: 'assets/icons/skills_backend/django.svg'
+    },
+    // {
+    //   name: 'DRF',
+    //   src: 'assets/icons/skills_backend/drf.svg'
+    // },
+    {
+      name: 'Docker',
+      src: 'assets/icons/skills_backend/docker.svg'
+    },
+    {
+      name: 'SQL',
+      src: 'assets/icons/skills_backend/sql.svg'
+    },
+    {
+      name: 'Linux',
+      src: 'assets/icons/skills_backend/linux.svg'
+    },
+    {
+      name: 'Postman',
+      src: 'assets/icons/skills_backend/postman.svg'
+    }
+  ];
+
+  toggleStackIcons() {
+    if (!this.stackFrontend == true) {
+      this.stackFrontend = true;
+      this.stackBackend = false;
+    } 
+    else {
+      this.stackFrontend = false;
+      this.stackBackend = true;
+    }
+  }
+
+  showFrontendIcons() {
+    if (!this.stackFrontend == true) {
+      this.stackFrontend = true;
+      this.stackBackend = false;
+    } 
+    // else {
+    //   this.stackFrontend = false;
+    //   this.stackBackend = true;
+    // }
+  }
+
+  showBackendIcons() {
+    if (this.stackFrontend == true) {
+      this.stackFrontend = false;
+      this.stackBackend = true;
+    } 
+    // else {
+    //   this.stackFrontend = true;
+    //   this.stackBackend = false;
+    // }
+  }
+
 }
