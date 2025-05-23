065afebdf1ba3a1b9403448d1d7845a8a4b0c56d
Anna Fritz <anna_eida@gmx.de>
2025-05-23 23:01:38 +0200

    feat: add falling dot effect on navbar elements

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -18,5 +18,36 @@ export class HeaderComponent {
   project = inject(ProjectdataService);
   translate = inject(TranslationService);
 
+  onHover(event: MouseEvent) {
+    const target = event.currentTarget as HTMLElement;
+    const dot = target.querySelector('.hover-dot') as HTMLElement;
+    if (!dot) return;
+    dot.style.visibility = 'visible';
+  }
+
+  onLeave(event: MouseEvent) {
+    const target = event.currentTarget as HTMLElement;
+    const dot = target.querySelector('.hover-dot') as HTMLElement;
+  
+    if (!dot) return;
+  
+    // Coordinates of the dot in the viewport
+    const rect = dot.getBoundingClientRect();
+  
+    // create copy of the dot
+    const fallingDot = dot.cloneNode(true) as HTMLElement;
+    fallingDot.classList.add('falling-dot');
+  
+    // Set the exact position (in relation to viewport)
+    fallingDot.style.left = `${rect.left + rect.width / 2 - 3}px`; // -3 = half width of dot
+    fallingDot.style.top = `${rect.top + window.scrollY}px`; // scrollY for correct position
+    dot.style.visibility = `hidden`;
+
+    document.body.appendChild(fallingDot);
+    setTimeout(() => {
+      fallingDot.remove();
+    }, 5000);
+  }
+
 }
 
